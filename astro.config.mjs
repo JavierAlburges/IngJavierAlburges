30a1891b466f6515db23fe59cd4423e4f0805864
Javier Alburges <javieralburges@gmail.com>
2024-08-22 23:09:04 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
-import netlify from "@astrojs/netlify";
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  output: "server",
-  adapter: netlify()
-});
+  site: 'https://javieralburges.netlify.app/'
+})
