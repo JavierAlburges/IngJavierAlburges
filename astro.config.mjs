67324fe964bec3c335fcfa904b9574539bd11332
Javier <javieralburges@gmail.com>
2024-08-22 22:36:50 -0400

    Se agrego netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: 'https://porfolio.dev/',
+  output: "server",
+  adapter: netlify()
+});
