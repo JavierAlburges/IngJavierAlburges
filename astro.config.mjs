c16cb1beb7c7250c368c15e65c1ca2899054c51a
JavierAlburges <javieralburges@gmail.com>
2025-08-14 10:49:51 -0400

    Se creo el sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://javieralburges.netlify.app/'
-})
+  integrations: [tailwind(), robotsTxt(), sitemap()],
+  site: 'https://javieralburges.vercel.app/'
+});
