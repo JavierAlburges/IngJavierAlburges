3c97b48d1964d00db04fd6bad5e4cbd758825fbb
Javier Alburges <javieralburges@gmail.com>
2026-02-19 20:57:15 -0400

    Se arreglo un error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
+
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), sitemap()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://javieralburges.vercel.app/'
 });
